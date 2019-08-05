b53b40f3120b42e2557e88cd061371e251754efb
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2019-08-05 12:06:12 +0700

    gitignore & app assets fix

---
+++ assets/AppAsset.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+        ['css/print.css', 'media' => 'print'],
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
